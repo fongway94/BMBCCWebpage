79b4bacc279435588aa768ba389070dfbbfe83dc
fongway94 <fongway94@gmail.com>
2026-07-14 14:51:37 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Using relative paths for easy deployment on GitHub Pages or other free static hosting
+  base: '/BMBCCWebpage/', // Tells Vite that the site is hosted at github.io/BMBCCWebpage/
 })
