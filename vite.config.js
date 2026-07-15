a888489b8925a7dab73290ba9cb5bbb73d443f86
fongway94 <fongway94@gmail.com>
2026-07-15 17:59:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// vite.config.js
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // ← Change this to root
+  base: '/', // Root path for Cloudflare Pages
 })
