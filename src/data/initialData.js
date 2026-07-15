22007da8c81bbf41177d630f804e3a895f336630
fongway94 <fongway94@gmail.com>
2026-07-15 19:25:30 +0800

    Auto-save: update site data [2026-07-15 11:25:29]

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -2,7 +2,7 @@ export const initialData = {
   "settings": {
     "churchName": {
       "zh": "大山脚浸信教会",
-      "en": "Bukit Mertajam Baptist Church Chinese"
+      "en": "BMBCC"
     },
     "churchAbbreviation": "BMBCC",
     "slogan": {
