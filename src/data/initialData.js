9b429620a84f83142deea9590f38d0b7d8241723
fongway94 <fongway94@gmail.com>
2026-07-15 10:35:43 +0800

    Auto-save: update site data [2026-07-15 02:35:42]

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -280,7 +280,7 @@ export const initialData = {
         "zh": "7979 吗哪食物银行 🐟",
         "en": "7979 Food Bank 🐟"
       },
-      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3SVf1SHMs72QWWD3mF2GHVBrQt6rsECzvJg5iyC9okg&s=10",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU7XuB-72CyprQCD8NFdgcrnY-zfOpbJeX-pxR3G8Iqg&s=10",
       "description": {
         "zh": "由教会同工与义工团队爱心筹备，为百利镇社区里处于贫困、残疾、孤寡或行动不便的弱势群体，提供每日/每周的营养爱心便当派送服务。透过这份看似微薄的便当，传递神无限的顾念与无限的供给。",
         "en": "Lovingly prepared by our staff and volunteer teams, this ministry provides nutritious bento meal deliveries to low-income, disabled, solitary elderly, or mobility-impaired individuals. Through these simple boxes of meals, we convey God's unlimited care and provision."
