0ba46f606635bcfb03a420e049d109a80f236478
fongway94 <fongway94@gmail.com>
2026-07-15 19:25:49 +0800

    Auto-save: update site data [2026-07-15 11:25:48]

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -2,7 +2,7 @@ export const initialData = {
   "settings": {
     "churchName": {
       "zh": "大山脚浸信教会",
-      "en": "BMBCCC"
+      "en": "BMBCC"
     },
     "churchAbbreviation": "BMBCC",
     "slogan": {
