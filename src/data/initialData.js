3a37c4f942bddfa4f00d931cb674570e9faac79b
fongway94 <fongway94@gmail.com>
2026-07-18 00:01:37 +0800

    Auto-save: update site data [2026-07-17 16:01:35]

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -939,7 +939,7 @@ export const initialData = {
     {
       "id": 3,
       "name": {
-        "zh": "BMBCC 南区聚会点",
+        "zh": "BKBC ",
         "en": "BMBCC South District Fellowship Point"
       },
       "googleMapsEmbedUrl": "https://maps.google.com/maps?q=Taman%20Alma%2C%2014000%20Bukit%20Mertajam%2C%20Penang%2C%20Malaysia&output=embed",
