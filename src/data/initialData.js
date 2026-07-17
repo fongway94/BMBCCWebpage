8062a3ac316b5df45479c8799827469c18c6c60d
fongway94 <fongway94@gmail.com>
2026-07-18 00:04:28 +0800

    Auto-save: update site data [2026-07-17 16:04:27]

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -953,7 +953,7 @@ export const initialData = {
       },
       "landmarks": {
         "zh": "附近地标：\n• Eco Bloom 公寓 (200米)\n• My News 便利店 (100米)",
-        "en": "Nearby Landmarks:\n• Taman Alma Commercial Centre (200m)\n• My News Convenience Store (100m)"
+        "en": "Nearby Landmarks:\n• Eco Bloom Condo (200m)\n• My News Convenience Store (100m)"
       }
     }
   ]
